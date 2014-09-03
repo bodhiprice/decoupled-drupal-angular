9347d0a403d64b423a77acbf7ddfe48f7770cef6
John Hannah <john@friendlymachine.net>
2014-09-03 14:59:09 -0400

    add controllers for front and about pages

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// practice app for decoupled or "Headless" Drupal
+
+angular
+  .module('app', ['ui.router'])
+  .controller('MainController', ['$scope', function($scope) {
+    // something here
+
+  }]);
